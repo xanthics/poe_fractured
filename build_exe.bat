968230a0d657675dcf84cbd384d6aa5fdeb04afb
xanthics <3137623+xanthics@users.noreply.github.com>
2019-03-31 22:29:38 -0700

    Removed exe from github commit, will still be in releases

--- build_exe.bat
+++ build_exe.bat
@@ -1,2 +1,3 @@
-pyinstaller --clean -F -w fractured_watcher.pyw
+pyinstaller --clean -F -w -n fractured_watcher_x86-64.single_file.exe xangui.pyw
+pyinstaller --clean -D -w -n fractured_watcher_x86-64.multi_file xangui.pyw
 pause
